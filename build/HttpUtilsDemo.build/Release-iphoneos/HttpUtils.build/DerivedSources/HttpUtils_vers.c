fe5f417775a97b629e67699dd5cec9c8e33a95a1
zhujilong <zhujilong2007@sina.com>
2019-06-19 13:52:09 +0800

    输出文档

---
+++ build/HttpUtilsDemo.build/Release-iphoneos/HttpUtils.build/DerivedSources/HttpUtils_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char HttpUtilsVersionString[];
+ extern const double HttpUtilsVersionNumber;
+
+ const unsigned char HttpUtilsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:HttpUtils  PROJECT:HttpUtilsDemo-1" "\n";
+ const double HttpUtilsVersionNumber __attribute__ ((used)) = (double)1.;
